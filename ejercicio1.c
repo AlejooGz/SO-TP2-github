a53b343b3a9d14e7b41b4fb01a0e844c86320357
AlejooGz <ale.maxigonzalez@gmail.com>
2023-04-07 23:00:15 -0300

    Ejercicio 1 en proceso...

---
+++ ejercicio1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int cant = 0;
+
+void manejador (int num){
+
+	printf("Recibi señal\n");
+	cant++;
+
+	if(cant == 3){
+		printf("Finalizo mi ejecucion\n");
+		exit(1);
+	}
+}
+
+int main(){
+
+	signal(2, manejador);
+	while(1);
+}
